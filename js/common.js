1a2d591a95895489a95eac2f5fb2e310b560bd5f
”Frederik <fred295q@stud.kea.dk>
2024-11-25 13:19:58 +0100

    header and footer

--- js/common.js
+++ js/common.js
@@ -3,3 +3,9 @@ export const BASE_URL = "http://localhost:8080";
 export const loggedUserID = () => {
   return sessionStorage.getItem("food_repo_user_id") || 0;
 };
+
+document.querySelector(".burgertoggle").addEventListener("click", () =>{
+  document.querySelector(".burgernav").classList.toggle("active")
+  document.querySelector(".burger").classList.toggle("hidden")
+  document.querySelector(".cross").classList.toggle("hidden")
+})
